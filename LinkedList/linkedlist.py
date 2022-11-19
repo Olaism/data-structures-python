baa2f3fe968d77e76650e0d1e6f7a696aa2c4c40
olaism <olaism01@gmail.com>
2022-11-19 04:14:09 -0800

    added add_right method and test

--- LinkedList/linkedlist.py
+++ LinkedList/linkedlist.py
@@ -7,10 +7,10 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
-    def show(self):
+    def datas(self):
         """ Show the data in the linked list """
 
-        if not self.head:
+        if not self.has_header():
             return []
         else:
             values = []
@@ -20,8 +20,28 @@ class LinkedList:
                 n = n.ref
             return values
 
+    def has_header(self):
+        if not self.head:
+            return False
+        return True
+
+    def get_last_element(self):
+        n = self.head
+        try:
+            while n is not None:
+                if n.ref is None:
+                    return n
+                n = n.ref
+        except AttributeError:
+            return ""
+
     def add_left(self, data):
         """ Add a node at the the beginning of the list """
         new_node = Node(data)  # create a new node
         new_node.ref = self.head  # The new node refrence to the head of the list
         self.head = new_node  # The new node is made the head of the list
+
+    def add_right(self, data):
+        """ Add a node at the end of the list """
+        new_node = Node(data)  # create a new node
+        self.get_last_element().ref = new_node
--- LinkedList/tests.py
+++ LinkedList/tests.py
@@ -5,23 +5,55 @@ from linkedlist import LinkedList
 
 class NodeTest(unittest.TestCase):
 
+    def setUp(self):
+        self.node = Node(300)
+
     def test_node_instance(self):
-        node = Node(300)
+        node = self.node
         self.assertIsInstance(node, Node)
 
     def test_node_data(self):
-        node = Node(400)
-        self.assertEqual(node.data, 400)
-        self.assertNotEqual(node.data, 300)
+        self.assertEqual(self.node.data, 300)
+        self.assertNotEqual(self.node.data, 200)
 
     def test_node_ref(self):
-        node = Node(200)
-        self.assertIsNone(node.ref)
+        self.assertIsNone(self.node.ref)
 
     def test_valid_node_ref(self):
-        node = Node(200)
-        node.ref = 'foo'
-        self.assertEqual(node.ref, 'foo')
+        self.node.ref = 'foo'
+        self.assertEqual(self.node.ref, 'foo')
+
+
+class LinkedListTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.node = Node(400)
+        self.llist = LinkedList()
+
+    def test_list_instance(self):
+        self.assertIsInstance(self.llist, LinkedList)
+
+    def test_list_no_data(self):
+        self.assertEqual(self.llist.datas(), [])
+
+    def test_list_head(self):
+        self.llist.head = self.node
+        self.assertTrue(self.llist.has_header())
+        self.assertEqual(self.llist.datas(), [400])
+
+    def test_add_left_method(self):
+        self.llist.head = self.node
+        self.llist.add_left(300)
+        self.assertTrue(self.llist.has_header())
+        self.assertEqual(self.llist.head.data, 300)
+        self.assertEqual(self.llist.datas(), [300, 400])
+
+    def test_add_right_method(self):
+        self.llist.head = self.node
+        self.llist.add_right(300)
+        self.assertTrue(self.llist.has_header())
+        self.assertEqual(self.llist.get_last_element().data, 300)
+        self.assertEqual(self.llist.datas(), [400, 300])
 
 
 if __name__ == '__main__':
